518b4ca01907d41bbf385ec1c260c579f9ac1cef
Tyson Taylor <mellowt@gmail.com>
2016-02-29 10:52:04 -0700

    cleaning up file bloat

--- public/js/controllers/adminCtrl.js
+++ public/js/controllers/adminCtrl.js
@@ -261,35 +261,3 @@ angular.module( "GamifyDevMountain" )
 
 
   } );
-
-
-
-
-
-
-
-
-
-/* Cards */
-angular.module( "GamifyDevMountain" )
-  .controller( "cardCtrl", function ( $scope ) {
-    $scope.adminCtrl = "cardCtrl";
-  } );
-
-/* Student */
-angular.module( "GamifyDevMountain" )
-  .controller( "studentCtrl", function ( $scope ) {
-    $scope.adminCtrl = "studentCtrl";
-  } );
-
-/* Cohort */
-angular.module( "GamifyDevMountain" )
-  .controller( "cohortCtrl", function ( $scope ) {
-    $scope.adminCtrl = "cohortCtrl";
-  } );
-
-/* Badge Request */
-angular.module( "GamifyDevMountain" )
-  .controller( "requestCtrl", function ( $scope ) {
-    $scope.adminCtrl = "requestCtrl";
-  } );
--- public/js/controllers/profileCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module("GamifyDevMountain").controller("profileCtrl", function($scope) {
-
-});
